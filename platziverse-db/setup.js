02c77298a1b28e45aca5ade50af89a46f03f4431
Alex Chip <alexchip32@gmail.com>
2020-07-26 18:59:44 -0600

    Creando advertencia sobre borrado de DB

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,8 +1,23 @@
 'use strict'
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule()
+
 async function setup () {
+  const answer = await prompt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?'
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
   const config = {
     database: process.env.DN_NAME || 'platziverse',
     username: process.env.DB_USER || 'platzi',
@@ -19,7 +34,7 @@ async function setup () {
 }
 
 function handleFatalError (err) {
-  console.error(err.message)
+  console.error(`${chalk.red('[Fatal error]')} ${err.message}`)
   console.error(err.stack)
   process.exit(1) // se mata el proceso pasandole como argumento el 1
 }
