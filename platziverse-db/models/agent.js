cc0fc5e703052f3e71124a65ffc179e4500bfd4d
Alex Chip <alexchip32@gmail.com>
2020-07-30 07:56:44 -0600

    Creacion de fixtures y definicion basica del servicio de Agent

--- platziverse-db/models/agent.js
+++ platziverse-db/models/agent.js
@@ -1,7 +1,7 @@
 'use strict'
 
+const Sequelize = require('sequelize')
 const setupDatabase = require('../lib/db')
-const { Sequelize } = require('sequelize')
 
 module.exports = function setupAgentModel (config) {
   const sequelize = setupDatabase(config)
--- platziverse-db/tests/agent-tests.js
+++ platziverse-db/tests/agent-tests.js
@@ -4,6 +4,8 @@ const test = require('ava')
 const sinon = require('sinon')
 const proxyquire = require('proxyquire')
 
+const agentFixtures = require('./fixtures/agent')
+
 const config = {
   logging: function () {}
 }
@@ -12,8 +14,9 @@ let MetricStub = {
   belongsTo: sinon.spy()
 }
 
+let single = Object.assign({}, agentFixtures.single)
+let id = 1
 let AgentStub = null
-
 let db = null
 let sandbox = null
 test.beforeEach(async () => {
@@ -44,4 +47,10 @@ test.serial('Setup', t => {
   t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be the MetricModel')
   t.true(MetricStub.belongsTo.called, 'Metric.belongsTo was executed')
   t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be the AgentModel')
+})
+
+test.serial('Agent#findById', async t => {
+  let agent = await db.Agent.findById(id)
+
+  t.deepEqual(agent, agentFixtures.byId(id), 'should be the same')
 })
